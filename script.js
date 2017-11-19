fd3f4ee169d5877b73938865ee0fb4d83fa59ea6
vanjaveselinovic <vanja.veselinovic.95@gmail.com>
2017-11-18 20:41:37 -0500

    better way to pass parameters

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ $(document).ready(function () {
 			$('#test-loading').css('display','block');
 			loading = true;
 
-			$.getJSON('http://127.0.0.1:5000/python/simulator/1024')
+			$.getJSON('http://127.0.0.1:5000/python/simulator',
+				{
+					 packetSize: 1024
+				})
 				.done(function(data, textStatus, jqXHR) {
 					loading = false;
 					console.log(data);
