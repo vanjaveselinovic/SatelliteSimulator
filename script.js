46bca068eb08c2a3fea2aa17ebeff9c12783a6a5
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2017-11-22 16:51:10 -0600

    add single satellite as Placemark

--- script.js
+++ script.js
@@ -15,11 +15,65 @@ $(document).ready(function () {
 	var wwd = new WorldWind.WorldWindow('canvas');
 
     wwd.addLayer(new WorldWind.BMNGOneImageLayer());
-    wwd.addLayer(new WorldWind.BingAerialWithLabelsLayer());
+    //wwd.addLayer(new WorldWind.BingAerialWithLabelsLayer());
 
-    wwd.addLayer(new WorldWind.CompassLayer());
-    wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
-    wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+    //wwd.addLayer(new WorldWind.CompassLayer());
+    //wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
+    //wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+
+    var placemark,
+        placemarkAttributes = new WorldWind.PlacemarkAttributes(null),
+        highlightAttributes,
+        placemarkLayer = new WorldWind.RenderableLayer("Placemarks"),
+        latitude = 45.4215,
+        longitude = -75.6972,
+        altitude = 1000000;
+
+    placemarkAttributes.drawLeaderLine = true;
+    placemarkAttributes.imageScale = 1;
+    placemarkAttributes.imageOffset = new WorldWind.Offset(
+        WorldWind.OFFSET_FRACTION, 0.5,
+        WorldWind.OFFSET_FRACTION, 0.5);
+    placemarkAttributes.imageColor = WorldWind.Color.WHITE;
+
+    var canvas = document.createElement("canvas"),
+        ctx2d = canvas.getContext("2d"),
+        size = 20;
+
+    canvas.width = size;
+    canvas.height = size;
+
+    ctx2d.fillStyle = "rgb(255, 255, 255)";
+	ctx2d.fillRect(0, 0, size, size);
+
+	ctx2d.fillStyle = "rgb(0, 0, 0)";
+	ctx2d.fillRect(2, 2, size - 4, size - 4);
+
+	ctx2d.fillStyle = "rgb(128, 128, 128)";
+	ctx2d.fillRect(4, 4, size - 8, size - 8);
+
+    placemark = new WorldWind.Placemark(
+    		new WorldWind.Position(latitude, longitude, altitude),
+    		false,
+    		null);
+    placemark.altitudeMode = WorldWind.ABSOLUTE;
+
+    placemarkAttributes =
+    		new WorldWind.PlacemarkAttributes(placemarkAttributes);
+    placemarkAttributes.imageSource =
+    		new WorldWind.ImageSource(canvas);
+    placemark.attributes = placemarkAttributes;
+
+ 	highlightAttributes =
+    		new WorldWind.PlacemarkAttributes(placemarkAttributes);
+    highlightAttributes.imageScale = 1.2;
+    placemark.highlightAttributes = highlightAttributes;
+
+    placemarkLayer.addRenderable(placemark);
+	wwd.addLayer(placemarkLayer);
+
+	//var layerManger = new LayerManager(wwd);
+	var highlightController = new WorldWind.HighlightController(wwd);
 
 	$('#button-run').click(function() {
 		if (!loading) {
