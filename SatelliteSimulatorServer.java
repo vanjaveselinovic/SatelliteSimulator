2808e0325ca9864cce6a300d71e3d41b3d7f8d35
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2018-01-01 19:22:27 -0500

    connect web app and web server

--- SatelliteSimulatorServer.java
+++ SatelliteSimulatorServer.java
@@ -9,7 +9,7 @@ import com.sun.net.httpserver.HttpServer;
 public class SatelliteSimulatorServer {
 	public static void main(String[] args) throws Exception {
 		HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-		server.createContext("/test", new MyHandler());
+		server.createContext("/simulator", new MyHandler());
 		server.setExecutor(null);
 		server.start();
 	}
