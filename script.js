0c6333f5827ace20a784a3794d705b01ed0ce4aa
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2018-01-14 00:15:12 -0500

    globe

--- script.js
+++ script.js
@@ -18,198 +18,24 @@ $(document).ready(function () {
 		$($('.section.page')[$(this).data("menuItemNumber")-1]).addClass('active');
 	})
 
-	/* ---------- WEB WORLDWIND ---------- */
-
-	var wwd = new WorldWind.WorldWindow('canvas');
-
-	wwd.goToAnimator.animationFrequency = 0;
-	wwd.goToAnimator.travelTime = 0;
-	wwd.goTo(new WorldWind.Position(45.4215, -75.6972, 35000000));
-
-	var earthLayer = new WorldWind.BMNGOneImageLayer();
-
-    wwd.addLayer(earthLayer);
-    //wwd.addLayer(new WorldWind.BingAerialWithLabelsLayer());
-
-    //wwd.addLayer(new WorldWind.CompassLayer());
-    //wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
-    //wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
-
-    /* orbits */
-
-    var ringPositions = [];
-	var row = [];
-	var currLon = 0;
-	var mesh = null;
-
-	var red = new WorldWind.Color(1, 0, 0, 0.75);
-	var blue = new WorldWind.Color(0, 0, 1, 0.75);
-
-	var ringAttributes = [];
-
-	ringAttributes[0] = new WorldWind.ShapeAttributes(null);
-	ringAttributes[0].outlineColor = red;
-	ringAttributes[0].interiorColor = new WorldWind.Color(0, 0, 0, 0);
-	ringAttributes[0].applyLighting = false;
-
-	ringAttributes[1] = new WorldWind.ShapeAttributes(ringAttributes[0]);
-	ringAttributes[1].outlineColor = blue;
-
-	var ringLayer = new WorldWind.RenderableLayer();
-
-	/* satellites */
-
-    var placemarks,
-        placemarkAttributes = new WorldWind.PlacemarkAttributes(null),
-        highlightAttributes,
-        placemarkLayer = new WorldWind.RenderableLayer("Placemarks"),
-        testLayer = new WorldWind.RenderableLayer("Test"),
-        latitude = 45.4215,
-        longitude = -75.6972,
-        altitude = 1000000;
-
-    var MIN_LATITUDE = -90,
-        MAX_LATITUDE = 90,
-        MIN_LONGITUDE = -180,
-        MAX_LONGITUDE = 180;
-
-    placemarkAttributes.drawLeaderLine = true;
-    placemarkAttributes.leaderLineAttributes
-    		= new WorldWind.ShapeAttributes();
-    placemarkAttributes.leaderLineAttributes.outlineColor
-    		= new WorldWind.Color(1, 1, 1, 0.25);
-    placemarkAttributes.imageScale = 1;
-    placemarkAttributes.imageOffset = new WorldWind.Offset(
-        WorldWind.OFFSET_FRACTION, 0.5,
-        WorldWind.OFFSET_FRACTION, 0.5);
-    placemarkAttributes.imageColor = WorldWind.Color.WHITE;
-
-	placemarkAttributes =
-    		new WorldWind.PlacemarkAttributes(placemarkAttributes);
-    placemarkAttributes.imageSource =
-    		new WorldWind.ImageSource(CanvasIcon.Satellite({size: 30, outline: true}));
-
-    highlightAttributes =
-    		new WorldWind.PlacemarkAttributes(placemarkAttributes);
-    highlightAttributes.imageScale = 1.2;
-
-	var placemarks = [];
-	var currPlacemark;
+	/* ---------- GLOBE ---------- */
 
 	var numRings = $('#input-nr').val();
 	var numSatellitesPerRing = $('#input-nspr').val();
+	var altitude = $('#input-alt').val();
+	var orbitalPeriod = $('#input-per').val() * 60; //seconds
+	var timeScale = $('#input-ts').val();
 
-	var rings = [];
-	var currRing;
+	var rotationPeriod = 23*60*60 + 56*60 + 4; //earth's rotation in seconds
 
-	var r;
-
-	/* adding */
-
-	var globe = new WorldWind.Globe(new WorldWind.EarthElevationModel());
-
-	function configure() {
-		ringLayer.removeAllRenderables();
-		placemarkLayer.removeAllRenderables();
-
-		placemarks.length = 0;
-		rings.length = 0;
-
-		for (var i = 0; i < numRings*2; i++) {
-			ringPositions[i] = [];
-			lon = MIN_LONGITUDE + i * MAX_LONGITUDE*2 / (numRings*2);
-			for (var lat = -90; lat <= 90; lat += 90) {
-				ringPositions[i].push(new WorldWind.Position(lat, lon, 1000000));
-			}
-
-			rings.push(new WorldWind.Path(ringPositions[i], ringAttributes[i % 2]));
-
-			currRing = rings[rings.length - 1];
-
-			ringLayer.addRenderable(currRing);
-		}
-
-		for (var i = 0; i < numRings; i++) {
-			for (var j = 0; j < numSatellitesPerRing; j++) {
-				placemarks.push(new WorldWind.Placemark(
-			    		new WorldWind.Position(
-			    				MIN_LATITUDE + 2 * j * MAX_LATITUDE*2 / numSatellitesPerRing,
-			    				MIN_LONGITUDE + i * MAX_LONGITUDE*2 / (numRings*2),
-			    				altitude),
-			    		false,
-			    		null));
-
-	    		currPlacemark = placemarks[placemarks.length - 1];
-	    		currPlacemark.altitudeMode = WorldWind.ABSOLUTE;
-			    currPlacemark.attributes = placemarkAttributes;
-			    currPlacemark.highlightAttributes = highlightAttributes;
-
-			    placemarkLayer.addRenderable(currPlacemark);
-			}
-		}
-
-		/* xyz coordinates test placemarls */
-
-		var testPosition1 = new WorldWind.Position(0, 0, 0);
-		var testPosition2 = new WorldWind.Position(0, 0, 0);
-		var testPosition3 = new WorldWind.Position(0, 0, 0);
-		var testPosition4 = new WorldWind.Position(0, 0, 0);
-
-		globe.computePositionFromPoint(0, 0, 0, testPosition1);
-		globe.computePositionFromPoint(10000000, 0, 0, testPosition2);
-		globe.computePositionFromPoint(0, 10000000, 0, testPosition3);
-		globe.computePositionFromPoint(0, 0, 10000000, testPosition4);
-
-		var testPlacemark1 = new WorldWind.Placemark(testPosition1, false, null);
-		var testPlacemark2 = new WorldWind.Placemark(testPosition2, false, null);
-		var testPlacemark3 = new WorldWind.Placemark(testPosition3, false, null);
-		var testPlacemark4 = new WorldWind.Placemark(testPosition4, false, null);
-
-		testPlacemark1.altitudeMode = WorldWind.ABSOLUTE;
-		testPlacemark2.altitudeMode = WorldWind.ABSOLUTE;
-		testPlacemark3.altitudeMode = WorldWind.ABSOLUTE;
-		testPlacemark4.altitudeMode = WorldWind.ABSOLUTE;
-
-		testPlacemark1.attributes = placemarkAttributes;
-		testPlacemark2.attributes = placemarkAttributes;
-		testPlacemark3.attributes = placemarkAttributes;
-		testPlacemark4.attributes = placemarkAttributes;
-
-		testLayer.addRenderable(testPlacemark1);
-		testLayer.addRenderable(testPlacemark2);
-		testLayer.addRenderable(testPlacemark3);
-		testLayer.addRenderable(testPlacemark4);
-	}
-
-	configure();
-
-	wwd.addLayer(ringLayer);
-	wwd.addLayer(placemarkLayer);
-	//wwd.addLayer(testLayer);
-
-	var highlightController = new WorldWind.HighlightController(wwd);
-
-	/* live update */
-
-	var altitudeInput;
-
-	$('#input-alt').on('input', function() {
-		altitudeInput = $('#input-alt').val();
-
-		if (!isNaN(altitudeInput) && altitudeInput > 0) {
-			$('#input-alt').removeClass('invalid-input');
-
-			for (var i = 0; i < placemarks.length; i++) {
-				placemarks[i].position.altitude = altitudeInput;
-			}
-
-			wwd.redraw();
-		} else {
-			$('#input-alt').addClass('invalid-input');
-		}
+	var globe = new Globe({
+		numRings: numRings,
+		numSatellitesPerRing: numSatellitesPerRing,
+		altitude: altitude,
+		orbitalPeriod: orbitalPeriod
 	});
 
-	/* ---------- CHOREOGRAPHING ---------- */
+	/* live update */
 
 	var numRingsInput;
 
@@ -218,8 +44,8 @@ $(document).ready(function () {
 
 		if (!isNaN(numRingsInput) && numRingsInput > 0) {
 			$('#input-nr').removeClass('invalid-input');
-			numRings = numRingsInput;
-			configure();
+			globe.numRings = numRingsInput;
+			globe.configure();
 		} else {
 			$('#input-nr').addClass('invalid-input');
 		}
@@ -232,10 +58,28 @@ $(document).ready(function () {
 
 		if (!isNaN(numSatellitesPerRingInput) && numSatellitesPerRingInput > 0) {
 			$('#input-nspr').removeClass('invalid-input');
-			numSatellitesPerRing = numSatellitesPerRingInput;
-			configure();
+			globe.numSatellitesPerRing = numSatellitesPerRingInput;
+			globe.configure();
 		} else {
 			$('#input-nspr').addClass('invalid-input');
+		}
+	});
+
+	var altitudeInput;
+
+	$('#input-alt').on('input', function() {
+		altitudeInput = $('#input-alt').val();
+
+		if (!isNaN(altitudeInput) && altitudeInput > 0) {
+			$('#input-alt').removeClass('invalid-input');
+
+			for (var i = 0; i < globe.placemarks.length; i++) {
+				globe.placemarks[i].position.altitude = altitudeInput;
+			}
+
+			globe.wwd.redraw();
+		} else {
+			$('#input-alt').addClass('invalid-input');
 		}
 	});
 
@@ -265,15 +109,12 @@ $(document).ready(function () {
 		}
 	});
 
+	/* ---------- CHOREOGRAPHING ---------- */
+
 	var deltaTimeMillis = 0, prevTimeMillis = performance.now();
 	var i = 0;
 	var j = 0;
 	var k = 0;
-
-	var orbitalPeriod = $('#input-per').val() * 60; //seconds
-	var timeScale = $('#input-ts').val();
-
-	var rotationPeriod = 23*60*60 + 56*60 + 4; //earth's rotation in seconds
 
 	var plusMinus = 1;
 
@@ -281,31 +122,31 @@ $(document).ready(function () {
 		deltaTimeMillis = currTimeMillis - prevTimeMillis;
 		prevTimeMillis = currTimeMillis;
 
-		ringLayer.removeAllRenderables();
+		globe.ringLayer.removeAllRenderables();
 
-		for (i = 0; i < rings.length; i++) {
-			for (j = 0; j < rings[i].positions.length; j++) {
-				rings[i].positions[j].longitude
+		for (i = 0; i < globe.rings.length; i++) {
+			for (j = 0; j < globe.rings[i].positions.length; j++) {
+				globe.rings[i].positions[j].longitude
 						+= deltaTimeMillis/1000 * 360/rotationPeriod * timeScale;
 			}
 			//ringLayer.addRenderable(new WorldWind.Path(rings[i].positions, rings[i].attributes));
-			ringLayer.addRenderable(rings[i]);
+			globe.ringLayer.addRenderable(globe.rings[i]);
 		}
 
 		plusMinus = 1;
 
-		for (i = 0; i < placemarks.length; i++) {
+		for (i = 0; i < globe.placemarks.length; i++) {
 			//if (i % numSatellitesPerRing === 0)
 			//	plusMinus = plusMinus*-1;
 
-			placemarks[i].position.latitude
+			globe.placemarks[i].position.latitude
 					+= (deltaTimeMillis/1000 * 360/orbitalPeriod * timeScale * plusMinus);
 
-			placemarks[i].position.longitude
+			globe.placemarks[i].position.longitude
 					+= (deltaTimeMillis/1000 * 360/rotationPeriod * timeScale);
 		}
 
-		wwd.redraw();
+		globe.wwd.redraw();
 
 		window.requestAnimationFrame(doFrame);
 	}
