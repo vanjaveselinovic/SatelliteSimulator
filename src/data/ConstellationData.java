d9046df51de495fe6b1706cbcaba41a635fae58d
xlirate <xlirate@live.ca>
2018-02-26 16:57:23 -0500

    adding data classes

---
+++ src/data/ConstellationData.java
@@ -0,0 +1,20 @@
+package data;
+
+public class ConstellationData {
+	/*
+	 * give me sattelite[0][0]'s orbit
+	 * 
+	 * remember to give it a bit of eccentricity so that they don't collide
+	 * also argument or pariapsis
+	 * 
+	 * How many sats per ring
+	 * how many rings
+	 * what's the offset of the orbits between the rings?
+	 * 
+	 * give me the time-and-date of the start of the simulator
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+}
---
+++ src/data/GroundStationData.java
@@ -0,0 +1,16 @@
+package data;
+
+public class GroundStationData {
+	
+	/*
+	 * lat, long
+	 * 
+	 * what other ground stations is this connected to?
+	 * What packet sending interval distrobution?
+	 * 
+	 * when does it start? defaults to 0
+	 * when does it end? used to pick a simulation length?
+	 * 
+	 * does it move?
+	 */
+}
